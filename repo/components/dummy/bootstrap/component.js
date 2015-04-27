b4c4d522ac429b913ed5dd18e7646ab719ff8bf3
clonq <ovi.comes@gmail.com>
2015-04-26 22:01:20 -0400

    container refactoring

--- repo/components/dummy/bootstrap/component.js
+++ repo/components/dummy/bootstrap/component.js
@@ -3,6 +3,7 @@ module.exports = function(){
     var _ = require('underscore');
 
     this.init = function(config) {
+console.log('bootstrap:', config)    	
         var that = this;
         this.params = _.defaults(config||{}, defaults)
         setTimeout(function() { process.emit(that.params.emit, that.params.payload); }, that.params.delay);
